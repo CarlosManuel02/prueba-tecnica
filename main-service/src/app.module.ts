5c2998b14514a05e145794aeab6f7912250defc4
Carlos Manuel Cedeño Barrera <carlosmcb02@gmail.com>
2025-08-03 14:46:04 -0400

    feat: env

--- main-service/src/app.module.ts
+++ main-service/src/app.module.ts
@@ -1,7 +1,13 @@
-import { Module } from "@nestjs/common"
-import { ExchangeModule } from "./exchange/exchange.module"
+import { Module } from '@nestjs/common';
+import { ExchangeModule } from './exchange/exchange.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [ExchangeModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    ExchangeModule,
+  ],
 })
 export class AppModule {}
--- main-service/src/exchange/exchange.service.ts
+++ main-service/src/exchange/exchange.service.ts
@@ -1,17 +1,48 @@
 import { Injectable } from '@nestjs/common';
 import axios from 'axios';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class ExchangeService {
-  constructor() {}
+  private readonly API1_URL: string;
+  private readonly API2_URL: string;
+  private readonly API3_URL: string;
 
+  constructor(private configService: ConfigService) {
+    // Utiliza ConfigService para obtener las URLs de las APIs
+    this.API1_URL = this.configService.get<string>(
+      'API1_URL',
+      'http://localhost:3001',
+    );
+    this.API2_URL = this.configService.get<string>(
+      'API2_URL',
+      'http://localhost:3002',
+    );
+    this.API3_URL = this.configService.get<string>(
+      'API3_URL',
+      'http://localhost:3003',
+    );
+  }
+  /**
+   * Extracts exchange rates from multiple APIs.
+   * @param sourceCurrency The currency to convert from.
+   * @param targetCurrency The currency to convert to.
+   * @param amount The amount to convert.
+   * @returns An array of conversion results from the APIs.
+   */
   async extracted(
     sourceCurrency: string,
     targetCurrency: string,
     amount: number,
-  ) {
+  ): Promise<any[]> {
+
+    /**
+     *Input {from, to, value}
+     * Output {rate}
+     */
     const api1 = axios
-      .post('http://api1-service:3001/api1', {
+      .post(`${this.API1_URL}/api1`, {
+        // Use configurable URL
         sourceCurrency: sourceCurrency,
         targetCurrency: targetCurrency,
         amount: amount,
@@ -22,9 +53,13 @@ export class ExchangeService {
         convertedAmount: amount * res.data.rate,
       }));
 
+    /**
+     * Input <XML><From/><To/><Amount/></XML>
+     * Output <XML><Result/></XML>
+     */
     const api2 = axios
       .post(
-        'http://api2-service:3002/api2',
+        `${this.API2_URL}/api2`,
         `<XML><From>${sourceCurrency}</From><To>${targetCurrency}</To><Amount>${amount}</Amount></XML>`,
         { headers: { 'Content-Type': 'application/xml' } },
       )
@@ -37,8 +72,12 @@ export class ExchangeService {
         };
       });
 
+    /**
+     * Input{exchange: {sourceCurrency,targetCurrency, quantity}}
+     * Output {statusCode, message, data: {total}}
+     */
     const api3 = axios
-      .post('http://api3-service:3003/api3', {
+      .post(`${this.API3_URL}/api3`, {
         exchange: {
           sourceCurrency,
           targetCurrency,
